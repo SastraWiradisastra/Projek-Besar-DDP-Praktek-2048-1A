0a89c6d84730452c0e5204e2babd2f4c526731ee
qinthcld <71803467+qinthcld@users.noreply.github.com>
2024-12-01 00:54:52 +0700

    game over jangan pointer

--- papan.h
+++ papan.h
@@ -8,7 +8,7 @@ void init(int board[u][u]);
 void displayBoard(int board[u][u]);
 bool move(int board[u][u], char direction);
 void addNewTile(int *board[u][u]);
-bool isGameOver(int *board[u][u]);
+bool isGameOver(int board[u][u]);
 void transpose(int *board[u][u]);
 void reverse(int *board[u][u]);
 void check(int board[u][u], int *jumlahscore);
