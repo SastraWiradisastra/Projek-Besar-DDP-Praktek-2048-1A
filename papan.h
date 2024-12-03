e1d93a0bf0147f8db830142edb56631265dca8ec
qinthcld <71803467+qinthcld@users.noreply.github.com>
2024-12-03 07:08:00 +0700

    pointer

--- papan.h
+++ papan.h
@@ -7,11 +7,11 @@
 void init(int board[u][u]);
 void displayBoard(int board[u][u]);
 bool move(int board[u][u], char direction);
-void addNewTile(int *board[u][u]);
+void addNewTile(int board[u][u]);
 bool isGameOver(int board[u][u]);
-void transpose(int *board[u][u]);
-void reverse(int *board[u][u]);
-void check(int board[u][u], int *jumlahscore);
-int tampScore(int jumlah);
+void transpose(int board[u][u]);
+void reverse(int board[u][u]);
+bool check(int board[u][u]);
+int tampScore();
 
 #endif
