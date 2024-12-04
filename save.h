9a37c9ecffbeb7ea0f60627163ca97786fc5daca
ndowdl <ndoobulet03@gmail.com>
2024-12-04 09:37:14 +0700

    Update save.h

---
+++ save.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {int skor; char nama[15];} dataplayer;
+
+void inputscore(dataplayer input);
+void tampil();
+void sorting();
