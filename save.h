7c70620b507af18c9cb6cf79014b7dcc10d78dbf
Sastra <158163186+SastraWiradisastra@users.noreply.github.com>
2024-12-07 02:13:57 +0000

    Update save.h

--- save.h
+++ save.h
@@ -1,9 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef SAVE_H
+#define SAVE_H
 
-typedef struct {int skor; char nama[15];} dataplayer;
+typedef struct {
+    int skor; 
+    char nama[15];
+} dataplayer;
 
 void inputscore(dataplayer input);
 void tampil();
 void sorting();
+
+#endif
